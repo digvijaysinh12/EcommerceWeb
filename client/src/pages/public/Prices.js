208235ce0f9cf02a60dcf4d6c691880d8a2a0bd0
digvijaysinh12 <sarvaiyaraa040@gmail.com>
2025-11-09 19:33:23 +0530

    Updated price covert filter from dollar to indian inr

--- client/src/pages/public/Prices.js
+++ client/src/pages/public/Prices.js
@@ -1,33 +1,32 @@
 export const Prices = [
     {
-        _id:0,
-        name:"$0 to 19",
-        array:[0,19]
+        _id: 0,
+        name: "₹0 to ₹999",
+        array: [0, 999]
     },
     {
-        _id:1,
-        name:"$20 to 39",
-        array:[20,39]
+        _id: 1,
+        name: "₹1000 to ₹1999",
+        array: [1000, 1999]
     },
     {
-        _id:2,
-        name:"$40 to 59",
-        array:[40,59]
+        _id: 2,
+        name: "₹2000 to ₹2999",
+        array: [2000, 2999]
     },
     {
-        _id:3,
-        name:"$60 to 79",
-        array:[60,79]
+        _id: 3,
+        name: "₹3000 to ₹4999",
+        array: [3000, 4999]
     },
     {
-        _id:4,
-        name:"$80 to 99",
-        array:[80,99]
+        _id: 4,
+        name: "₹5000 to ₹6999",
+        array: [5000, 6999]
     },
     {
-        _id:5,
-        name:"$100 or more",
-        array:[100,9999]
+        _id: 5,
+        name: "₹7000 or more",
+        array: [7000, 999999]
     }
-    
 ]
